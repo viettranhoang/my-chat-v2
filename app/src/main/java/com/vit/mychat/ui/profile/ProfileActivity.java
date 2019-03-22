0c87bd43f30ff032578f4f7d7e74c8a5ce265132
heo <kimhu26@gmail.com>
2019-03-22 21:06:37 +0700

    add profile activity xml

--- app/src/main/java/com/vit/mychat/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/vit/mychat/ui/profile/ProfileActivity.java
@@ -2,11 +2,21 @@ package com.vit.mychat.ui.profile;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.ImageView;
 
 import com.vit.mychat.R;
 import com.vit.mychat.ui.base.BaseActivity;
+import com.vit.mychat.util.GlideApp;
+
+import butterknife.BindView;
 
 public class ProfileActivity extends BaseActivity {
+
+    @BindView(R.id.image_avatar)
+    ImageView mImageAvatar;
+
+    @BindView(R.id.image_background)
+    ImageView mImageBackground;
 
     public static void moveProfileActivity(Activity activity) {
         activity.startActivity(new Intent(activity, ProfileActivity.class));
@@ -19,6 +29,16 @@ public class ProfileActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        GlideApp.with(this)
+                .load(R.drawable.kim_avatar)
+                .circleCrop()
+                .into(mImageAvatar);
+
+        GlideApp.with(this)
+                .load(R.drawable.background_avatar)
+//                .centerCrop()
+                .into(mImageBackground);
+
         showToast("Hello");
     }
 }
