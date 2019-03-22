1e2ce4064f51d61fd601dde4379d7bcd33f5c76f
Vit <anhxtanhwp@gmail.com>
2019-03-23 03:21:52 +0700

    add profile activity

--- app/src/main/java/com/vit/mychat/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/vit/mychat/ui/profile/ProfileActivity.java
@@ -7,20 +7,21 @@ import android.widget.ImageView;
 import com.vit.mychat.R;
 import com.vit.mychat.ui.base.BaseActivity;
 import com.vit.mychat.util.GlideApp;
+import com.vit.mychat.util.RoundedCornersTransformation;
 
 import butterknife.BindView;
 
 public class ProfileActivity extends BaseActivity {
+
+    public static void moveProfileActivity(Activity activity) {
+        activity.startActivity(new Intent(activity, ProfileActivity.class));
+    }
 
     @BindView(R.id.image_avatar)
     ImageView mImageAvatar;
 
     @BindView(R.id.image_background)
     ImageView mImageBackground;
-
-    public static void moveProfileActivity(Activity activity) {
-        activity.startActivity(new Intent(activity, ProfileActivity.class));
-    }
 
     @Override
     protected int getLayoutId() {
@@ -30,15 +31,15 @@ public class ProfileActivity extends BaseActivity {
     @Override
     protected void initView() {
         GlideApp.with(this)
-                .load(R.drawable.kim_avatar)
+                .load("http://gnemart.com/wp-content/uploads/2018/10/gau-bong-quobee-1.jpg")
                 .circleCrop()
                 .into(mImageAvatar);
 
         GlideApp.with(this)
-                .load(R.drawable.background_avatar)
-//                .centerCrop()
+                .load("https://www.incimages.com/uploaded_files/image/970x450/getty_509107562_2000133320009280346_351827.jpg")
+                .centerCrop()
+                .transform(new RoundedCornersTransformation(30, 0, RoundedCornersTransformation.CornerType.TOP))
                 .into(mImageBackground);
 
-        showToast("Hello");
     }
 }
